04edabf2436c9b76ffb0795e1b767ce6bbedfb65
Chris Nixon <chris.d.nixon@gmail.com>
2015-05-08 11:19:12 -0700

    Use /

--- blueprints/ember-cli-pusher/index.js
+++ blueprints/ember-cli-pusher/index.js
@@ -3,6 +3,6 @@ module.exports = {
   },
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('ember-pusher', 'git://github.com:dasnixon/ember-pusher.git#use_ember_keys');
+    return this.addBowerPackageToProject('ember-pusher', 'git://github.com/dasnixon/ember-pusher.git#use_ember_keys');
   }
 };
